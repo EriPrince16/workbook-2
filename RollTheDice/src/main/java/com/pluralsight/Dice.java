8da744d428f462594050847c623a2c1fb6bc72a4
Million Bereket <million.bereket16@gmail.com>
2025-04-21 16:40:29 -0500

    Completed Roll The Dice

---
+++ RollTheDice/src/main/java/com/pluralsight/Dice.java
@@ -0,0 +1,41 @@
+package com.pluralsight;
+
+public class Dice {
+    public static void main(String[] args) {
+        Dice dice = new Dice();
+        int roll1, roll2;
+        int twoCounter = 0, fourCounter = 0, sixCounter = 0, sevenCounter = 0;
+
+        for (int i = 1; i <= 100; i++) {
+            roll1 = Dice.roll(1, 6);
+            roll2= Dice.roll(1, 6);
+            System.out.println("Roll " + i + (": ") + roll1 + " - " + roll2 + " Sum = " + (roll1 + roll2));
+
+            if (roll1 + roll2 == 2) {
+                twoCounter++;
+
+            }
+            if (roll1 + roll2 == 4) {
+                fourCounter++;
+            }
+            if (roll1 + roll2 == 6) {
+                sixCounter++;
+            }
+            if (roll1 + roll2 == 7) {
+                sevenCounter++;
+            }
+
+
+        }
+        System.out.println("Two Counter: "+ twoCounter + " " + "Four Counter: "+ fourCounter + " " + "SixCounter: "+ sixCounter + " " + "Seven Counter: "+ sevenCounter);
+
+
+
+
+    }
+
+    private static int roll(int min, int max) {
+        int randomNumber = (int)(Math.random() * max) + min;
+        return randomNumber;
+    }
+}
